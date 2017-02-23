f91c27bbd7084ea02b5db32a67a3909f18cff752
vincentwang901130 <vincentwang901130@me.com>
2017-02-23 13:54:36 -0500

    Readme and Code Style Refined

--- Movie_Trailer_Website/media.py
+++ Movie_Trailer_Website/media.py
@@ -1,14 +1,22 @@
 import webbrowser
-#move class
+
+
+# movie class
 class Movie():
-	#class init func
-	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-		self.title = movie_title
-		self.storyline = movie_storyline
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url= trailer_youtube
-	#call webbrowser.open func to show youtube trailer
-	def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)
+        # class init func
+	def __init__(self, movie_title,\
+                movie_storyline,\
+                poster_image, \
+                trailer_youtube):
+            # Movie title
+            self.title = movie_title
+            # Movie storyLine
+            self.storyline = movie_storyline
+            # Movie poster image url
+            self.poster_image_url = poster_image
+            # Movie trailer url
+            self.trailer_youtube_url = trailer_youtube
 
-
+	# call webbrowser.open func to show youtube trailer
+        def show_trailer(self):
+            webbrowser.open(self.trailer_youtube_url)
